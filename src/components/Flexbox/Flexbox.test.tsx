6adf08854462a25a7ce7733627dc2c9c8d9385e0
Carol Leung <carol.carl.leung@gmail.com>
2020-04-17 16:52:01 +1200

    fix: replace flexbox react component with styled component

--- src/components/Flexbox/Flexbox.test.tsx
+++ src/components/Flexbox/Flexbox.test.tsx
@@ -4,21 +4,14 @@ import { Flexbox } from "./Flexbox";
 
 const DATA = {
   CONTENT: <div>content</div>,
-  CLASSNAME: "flexbox",
 };
 
 describe("Flexbox", () => {
   describe("renders <Flexbox /> components", () => {
-    const wrapper = shallow(<Flexbox className={DATA.CLASSNAME}>{DATA.CONTENT}</Flexbox>);
-
-    it("should have a class name", () => {
-      expect(wrapper.hasClass(DATA.CLASSNAME)).toBe(true);
-    });
+    const wrapper = shallow(<Flexbox>{DATA.CONTENT}</Flexbox>);
 
     it("renders children when passed in", () => {
       expect(wrapper.contains(DATA.CONTENT)).toBe(true);
     });
-
-    // TODO: renders inline styles
   });
 });
