08248ffcbf3bd9c534060cb56ba7285bf190817a
Carol Leung <carol.carl.leung@gmail.com>
2020-04-17 17:15:30 +1200

    chore: add comment in flexbox story

--- src/components/Flexbox/Flexbox.stories.tsx
+++ src/components/Flexbox/Flexbox.stories.tsx
@@ -9,6 +9,7 @@ export default {
 // TODO: add Storybook AddOns info about styled-system,
 // add info about HOC to add inline style and classname
 // https://styled-system.com/guides/build-a-box
+// addd info about no Flexbox types for this component
 export const Default: React.FunctionComponent = () => (
   <Flexbox alignContent="center" justifyContent="space-around">
     This is a flexbox content powered by&nbsp;<a href="https://styled-system.com/api/">styled-system</a>.
