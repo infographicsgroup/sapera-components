3939ca6b0eb89ea867fb1686052eaf93f1077210
Carol Leung <carol.carl.leung@gmail.com>
2020-04-17 16:52:01 +1200

    fix: replace flexbox react component with styled component

--- src/components/Flexbox/Flexbox.tsx
+++ src/components/Flexbox/Flexbox.tsx
@@ -1,22 +1,9 @@
-import React, { FC } from "react";
-import styled, { CSSProperties } from "styled-components";
+import styled from "styled-components";
 import { flexbox, FlexboxProps } from "styled-system";
 
-export interface FlexboxComponentProps extends FlexboxProps {
-  children: React.ReactNode;
-  className?: string;
-  styles?: CSSProperties;
-}
-
-const FlexboxStyled = styled.div<FlexboxProps>(
+export const Flexbox = styled.div<FlexboxProps>(
   {
     display: "flex",
   },
   flexbox,
-);
-
-export const Flexbox: FC<FlexboxComponentProps> = ({ className, children, styles }: FlexboxComponentProps) => (
-  <FlexboxStyled className={className} style={styles}>
-    {children}
-  </FlexboxStyled>
 );
