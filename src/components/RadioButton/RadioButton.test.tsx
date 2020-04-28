aaa3d56d167835033011e72792a253bf4d787954
kaylarobertson3 <kayla.robertson3@gmail.com>
2020-04-28 19:37:03 +0200

    edit tests

--- src/components/RadioButton/RadioButton.test.tsx
+++ src/components/RadioButton/RadioButton.test.tsx
@@ -7,32 +7,36 @@ const DATA = {
   TEXT: "Text",
   VALUE: "Value",
   CLASSNAME: "radioButton",
+  CHECKED: true,
 };
 
 describe("RadioButon", () => {
   describe("Renders common props as expected", () => {
     const mockFn = jest.fn();
     const wrapper = shallow(
-      <RadioButton className={DATA.CLASSNAME} name={DATA.NAME} value={DATA.VALUE} onClick={mockFn}>
+      <RadioButton
+        checked={DATA.CHECKED}
+        className={DATA.CLASSNAME}
+        name={DATA.NAME}
+        value={DATA.VALUE}
+        onClick={mockFn}
+      >
         {DATA.TEXT}
       </RadioButton>,
     );
 
-    it("renders children as a string expected", () => {
-      expect(wrapper.children().text()).toEqual(DATA.TEXT);
-    });
+    console.log("wrapper", wrapper.props());
 
     it("should have a class name", () => {
       expect(wrapper.hasClass(DATA.CLASSNAME)).toBe(true);
     });
 
-    it("simulates click events", () => {
-      wrapper.find("button").simulate("click");
-      expect(mockFn.mock.calls.length).toEqual(1);
-    });
-
     it("should not be disabled", () => {
       expect(wrapper.props().disabled).toEqual(false);
+    });
+
+    it("should be checked", () => {
+      expect(wrapper.props().ischecked).toEqual(true);
     });
   });
 });
