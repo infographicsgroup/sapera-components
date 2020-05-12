f72eede86e8ff8e4aabc69e86266f753d42312b4
a14m <a.abdelhalim@i.graphics>
2020-05-12 18:04:45 +0200

    Only generate and bundle components types in the package
    Previously the package included all of the following files
    
    ```
    >>> npm notice 217B   dist/components/0.welcome.stories.d.ts
        npm notice 712B   dist/components/Button/Button.d.ts
    >>> npm notice 396B   dist/components/Button/Button.stories.d.ts
    >>> npm notice 12B    dist/components/Button/Button.test.d.ts
        npm notice 257B   dist/components/Flexbox/Flexbox.d.ts
    >>> npm notice 337B   dist/components/Flexbox/Flexbox.stories.d.ts
    >>> npm notice 12B    dist/components/Flexbox/Flexbox.test.d.ts
        npm notice 229B   dist/components/Button/Icon.d.ts
        npm notice 99B    dist/components/Button/index.d.ts
        npm notice 59B    dist/components/Flexbox/index.d.ts
        npm notice 124B   dist/components/RadioButton/index.d.ts
        npm notice 120B   dist/index.d.ts
        npm notice 427B   dist/components/RadioButton/RadioButton.d.ts
    >>> npm notice 273B   dist/components/RadioButton/RadioButton.stories.d.ts
    >>> npm notice 12B    dist/components/RadioButton/RadioButton.test.d.ts
    ```
    
    This indicated that the stories and test data types were also included
    in the bundled code...
    
    This code solves the problem by introducing dts bundling in just a
    single file...
    The output file (`dist/index.d.ts`) would only include the component
    defined data types, saving space and not introducing a code that not
    needed to be shipped with the code...
    
    Side effect, since all the data types are rolled in 1 file, it can be
    easier to see the type definitions together and search within the same
    file
    
    The output of the package should be the following files only
    ```
      dist/
      ├── index.d.ts
      ├── index.js
      └── index.js.map
    ```

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,20 @@
 import typescript from "rollup-plugin-typescript2";
+import dts from "rollup-plugin-dts";
 const pkg = require("./package.json");
 
-export default {
-  input: ["src/index.ts"],
-  output: [{ file: pkg.main, format: "cjs", sourcemap: true }],
-  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-  plugins: [typescript({ clean: true })],
-};
+export default [
+  // Typescript
+  {
+    input: ["src/index.ts"],
+    output: [{ file: pkg.main, format: "cjs", sourcemap: true }],
+    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+    plugins: [typescript({ clean: true })],
+  },
+  // Data type declarations
+  {
+    input: ["src/index.ts"],
+    output: [{ file: pkg.types, format: "es" }],
+    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+    plugins: [dts()],
+  },
+];
