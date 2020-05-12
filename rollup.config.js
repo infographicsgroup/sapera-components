cd3530ef305383f92405702b83de86523f00174f
a14m <a.abdelhalim@i.graphics>
2020-05-12 18:27:36 +0200

    Refactor external dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,22 @@ import typescript from "rollup-plugin-typescript2";
 import dts from "rollup-plugin-dts";
 const pkg = require("./package.json");
 
+// Only package.json dependencies section is considered internal, other dependencies should be external/excluded
+const externalDependencies = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
+
 export default [
   // Typescript
   {
     input: ["src/index.ts"],
     output: [{ file: pkg.main, format: "cjs", sourcemap: true }],
-    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+    external: externalDependencies,
     plugins: [typescript({ clean: true })],
   },
   // Data type declarations
   {
     input: ["src/index.ts"],
     output: [{ file: pkg.types, format: "es" }],
-    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+    external: externalDependencies,
     plugins: [dts()],
   },
 ];
