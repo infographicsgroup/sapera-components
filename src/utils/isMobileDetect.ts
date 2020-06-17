141dd6b1b9355444ac2eadefe2ba84d8bacbb6fc
Carol Leung <carol.carl.leung@gmail.com>
2020-06-17 20:56:32 +1200

    refactor: replace js with ts file

--- src/utils/isMobileDetect.ts
+++ src/utils/isMobileDetect.ts
@@ -1,7 +1,7 @@
 // http://www.detectmobilebrowsers.com
-export default function isMobile() {
+export default function isMobile(): boolean {
   let check = false;
-  (function (a) {
+  ((a) => {
     if (
       /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
         a,
@@ -12,6 +12,6 @@ export default function isMobile() {
     ) {
       check = true;
     }
-  })(navigator.userAgent || navigator.vendor || window.opera);
+  })(navigator.userAgent || navigator.vendor);
   return check;
 }
