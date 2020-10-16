c92984a110cceb890674e7163365fa0daca6c39c
kayla.robertson3 <kayla.robertson3@gmail.com>
2020-10-16 11:34:42 +0200

    style: center checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -2,7 +2,6 @@ import React, { FC } from "react";
 import styled from "styled-components";
 import { TickIcon } from "../Icon/Icons";
 import { Color } from "../../theme/util";
-import { Column } from "../../theme/custom-styled-components";
 
 const LABEL_SIZE = 24;
 const CHECKMARK_CLASSNAME = "checkmark";
@@ -11,9 +10,8 @@ const LabelStyled = styled.label`
   position: relative;
   display: flex;
   align-items: center;
-  flex-direction: row;
-  font-family: monospace;
   padding-left: ${LABEL_SIZE + 10}px;
+  font-family: monospace;
   -webkit-user-select: none;
   -moz-user-select: none;
   -ms-user-select: none;
@@ -25,8 +23,8 @@ const LabelStyled = styled.label`
 const InputStyled = styled.input`
   position: absolute;
   z-index: 2;
-  // top: -5px;
-  left: -2px;
+  top: -5px;
+  left: -5px;
   opacity: 0;
   width: ${LABEL_SIZE + 3}px;
   height: ${LABEL_SIZE + 3}px;
@@ -46,13 +44,12 @@ const InputStyled = styled.input`
 
 const CheckmarkStyled = styled.span`
   position: absolute;
-  z-index: 1;
-  top: 0;
   left: 0;
-  width: ${LABEL_SIZE}px;
-  height: ${LABEL_SIZE}px;
   display: flex;
   align-items: center;
+  width: ${LABEL_SIZE}px;
+  height: ${LABEL_SIZE}px;
+  z-index: 1;
   background-color: ${Color.BackgroundMain};
   border: 2px solid ${Color.Primary};
   border-radius: 5px;
@@ -68,39 +65,29 @@ export interface CheckboxProps {
   children: string | React.ReactNode;
   checked?: boolean;
   disabled?: boolean;
+  id: string;
   name: string;
   value: string;
-  id?: string;
-  onChange: (value: any) => void;
+  onInputChange: (value: any) => void;
 }
 
 export const Checkbox: FC<CheckboxProps> = ({
+  id,
   children,
   className,
-  onChange,
+  onInputChange,
   checked,
   name,
   value,
-  id,
   disabled = false,
 }: CheckboxProps) => {
   return (
-    <Column className={className}>
-      <LabelStyled htmlFor={name}>
-        <InputStyled
-          checked={checked}
-          disabled={disabled}
-          id={id || name}
-          name={name}
-          type="checkbox"
-          value={value}
-          onChange={onChange}
-        />
-        <CheckmarkStyled className={CHECKMARK_CLASSNAME}>
-          <TickIcon />
-        </CheckmarkStyled>
-        {children}
-      </LabelStyled>
-    </Column>
+    <LabelStyled className={className} htmlFor={id} onChange={onInputChange}>
+      <InputStyled checked={checked} disabled={disabled} id={id} name={name} type="checkbox" value={value} />
+      <CheckmarkStyled className={CHECKMARK_CLASSNAME}>
+        <TickIcon />
+      </CheckmarkStyled>
+      {children}
+    </LabelStyled>
   );
 };
